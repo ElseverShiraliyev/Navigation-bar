aa102d53b61a5a74a6a772ad89a3b100f473b1be
ElseverShiraliyev <125830875+ElseverShiraliyev@users.noreply.github.com>
2023-05-21 23:34:43 +0400

    Dashboard

---
+++ index.js
@@ -0,0 +1,99 @@
+// let mudurAdi = "Elsever";
+// let sinifkom = "Ilqar";
+
+// console.log("Mudur adi :" + mudurAdi);
+// console.log("Sinifkom adi :" + sinifkom);
+
+// const maas = 5500;
+// const vergi = 0.2;
+// let maaslivergi = maas + maas * vergi;
+// console.log(maaslivergi);
+
+// let js = "3";
+// let py = 3;
+
+// console.log(js === py);
+
+// let nums = [1, 2, 3];
+// nums[0] = 10;
+
+// console.log(nums);
+
+// let nums = [1, 2, 3];
+// let numbers = [1, 2, 3];
+
+// console.log(nums === numbers);
+
+// let userOne = {
+//   name: "Asabeneh",
+//   role: "teaching",
+//   country: "Finland",
+// };
+// console.log(userOne.name);
+
+// const PI = Math.PI; // Buraya kısa bir açıklama getirelim. Math objesi sayesinde PI sayısının değerini otomatik alıyoruz.
+
+// console.log(PI);
+
+// const PI = Math.PI // Buraya kısa bir açıklama getirelim. Math objesi sayesinde PI sayısının değerini otomatik alıyoruz.
+
+// console.log(PI)                            // 3.141592653589793
+
+// // En yakın sayıya yuvarlama
+// // Eğer .5'in altındaysa aşağıya üstündeyse yukarıya yuvarlar. Örn: 3.14 ise 3 yapar 3.51 ise 4 yapar.
+
+// console.log(Math.round(PI))                // 3.14 olduğu için 3 e yuvarlama yapıyor
+
+// console.log(Math.round(9.81))              // Ondası 0.5 üstünde olduğu için 10 yapar
+
+// console.log(Math.floor(PI))                // floor aşağı yuvarlar Bu 3 olur
+
+// console.log(Math.ceil(PI))                 // ceil yukarı yuvarlar Bu 4 olur
+
+// console.log(Math.min(-5, 3, 20, 4, 5, 10)) // En küçük sayıyı bulmaya yaradığı için sonuç -5 döner
+
+// console.log(Math.max(-5, 3, 20, 4, 5, 10)) // En büyük sayıyı bulmaya yaradığı için sonuç 20 döner
+
+// const randNum = Math.random() // 0 ile 0.999999 arasında rastgele bir sayı üretir.
+// console.log(randNum)
+
+// // 0 ile 10 arasında rastgele bir sayı oluşturalım.
+
+// const num = Math.floor(Math.random () * 11) // 0 ile 10 arasında rastgele sayı oluşturur
+// console.log(num)
+
+// //Mutlak değer
+// console.log(Math.abs(-10))      // 10
+
+// //Kare kök
+// console.log(Math.sqrt(100))     // 10
+
+// console.log(Math.sqrt(2))       // 1.4142135623730951
+
+// // Üs
+// console.log(Math.pow(3, 2))     // 9
+
+// console.log(Math.E)             // 2.718
+
+// // Logaritma
+// // Returns the natural logarithm with base E of x, Math.log(x)
+// console.log(Math.log(2))        // 0.6931471805599453
+// console.log(Math.log(10))       // 2.302585092994046
+
+// // Sırasıyla 2 ve 10'un doğal logaritmasını döndürür
+// console.log(Math.LN2)           // 0.6931471805599453
+// console.log(Math.LN10)          // 2.302585092994046
+
+// // Trigonometri
+// Math.sin(0)
+// Math.sin(60)
+
+// Math.cos(0)
+// Math.cos(60)
+
+// console.log("Days\tTopics\tExercises");
+// console.log("This is a backslash  symbol (\\)");
+
+let string = "JavaScript";
+
+console.log(string.substring(0, 4));
